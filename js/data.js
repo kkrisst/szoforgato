d4fe6a26de48723d0cc648e67817a3f2a97a377a
kkrisst <kristof.kover@koverkristof.LWR.local>
2017-01-23 18:15:01 +0100

    HTML, CSS and JS templates added.

---
+++ js/data.js
@@ -0,0 +1,157 @@
+
+var data = {
+	"data" : 
+	{
+		"bla1" : "bla1",
+		"bla2" : "bla2",
+		"bla3" : "bla3",
+		"bla4" : "bla4",
+		"lessons" : [
+				{
+					"title_eng" : "Fruits",
+					"title_hun" : "Gyümölcsök",
+					"description_eng" : "Lesson about fruits.",
+					"description_hun" : "Lecke a gyümölcsökről.",
+					"bla" : "bla",
+					"bla2" : "bla2",
+					"questions" : [
+						{
+							"seq_number" : "1",
+							"question_eng" : "Apple",
+							"question_hun" : "Alma",
+							"elements" : [
+								{ "element" : "a" },
+								{ "element" : "l" },
+								{ "element" : "m" },
+								{ "element" : "a" }
+							]
+						},
+						{
+							"seq_number" : "2",
+							"question_eng" : "Pear",
+							"question_hun" : "Körte",
+							"elements" : [
+								{ "element" : "k" },
+								{ "element" : "ö" },
+								{ "element" : "r" },
+								{ "element" : "t" },
+								{ "element" : "e" }
+							]
+						},
+						{
+							"seq_number" : "3",
+							"question_eng" : "Plum",
+							"question_hun" : "Szilva",
+							"elements" : [
+								{ "element" : "s" },
+								{ "element" : "z" },
+								{ "element" : "i" },
+								{ "element" : "l" },
+								{ "element" : "v" },
+								{ "element" : "a" }
+							]
+						}
+					]
+				},
+				{
+					"title_eng" : "Furniture",
+					"title_hun" : "Bútorok",
+					"description_eng" : "Lesson about furniture.",
+					"description_hun" : "Lecke a bútorokról.",
+					"bla" : "bla",
+					"bla2" : "bla2",
+					"questions" : [
+						{
+							"seq_number" : "1",
+							"question_eng" : "Table",
+							"question_hun" : "Asztal",
+							"elements" : [
+								{ "element" : "a" },
+								{ "element" : "s" },
+								{ "element" : "z" },
+								{ "element" : "t" },
+								{ "element" : "a" },
+								{ "element" : "l" }
+							]
+						},
+						{
+							"seq_number" : "2",
+							"question_eng" : "Chair",
+							"question_hun" : "Szék",
+							"elements" : [
+								{ "element" : "s" },
+								{ "element" : "z" },
+								{ "element" : "é" },
+								{ "element" : "k" }
+							]
+						},
+						{
+							"seq_number" : "3",
+							"question_eng" : "Shelf",
+							"question_hun" : "Polc",
+							"elements" : [
+								{ "element" : "p" },
+								{ "element" : "o" },
+								{ "element" : "l" },
+								{ "element" : "c" }
+							]
+						}
+					]
+				},
+				{
+					"title_eng" : "Animals",
+					"title_hun" : "Állatok",
+					"description_eng" : "Lesson about animals.",
+					"description_hun" : "Lecke az állatokról.",
+					"bla" : "bla",
+					"bla2" : "bla2",
+					"questions" : [
+						{
+							"seq_number" : "1",
+							"question_eng" : "Dog",
+							"question_hun" : "Kutya",
+							"elements" : [
+								{ "element" : "k" },
+								{ "element" : "u" },
+								{ "element" : "t" },
+								{ "element" : "y" },
+								{ "element" : "a" }								
+							]
+						},
+						{
+							"seq_number" : "2",
+							"question_eng" : "Cat",
+							"question_hun" : "Macska",
+							"elements" : [
+								{ "element" : "m" },
+								{ "element" : "a" },
+								{ "element" : "c" },
+								{ "element" : "s" },
+								{ "element" : "k" },
+								{ "element" : "a" }
+							]
+						},
+						{
+							"seq_number" : "3",
+							"question_eng" : "Elephant",
+							"question_hun" : "Elefánt",
+							"elements" : [
+								{ "element" : "e" },
+								{ "element" : "l" },
+								{ "element" : "e" },
+								{ "element" : "f" },
+								{ "element" : "á" },
+								{ "element" : "n" },
+								{ "element" : "t" }
+							]
+						}
+					]
+				}
+		]
+	}
+};
+
+function init() {
+	
+	console.log(data);
+}
